cba53a5bcd3fd092baa53da634795665f5ed3afc
mars-86 <hrslash86@gmail.com>
2024-05-28 18:14:25 -0300

    feat: added configure.ac README and Makefiles

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "networking.h"
+#include "lib/networking.h"
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
--- networking.h
+++
@@ -1,6 +0,0 @@
-#ifndef __NETWORKING_INCLUDED_H__
-#define __NETWORKING_INCLUDED_H__
-
-#include "lib/networking.h"
-
-#endif // __NETWORKING_INCLUDED_H__
